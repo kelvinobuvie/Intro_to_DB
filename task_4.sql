bac841980f2eb7d72cd98e763ce75fd1b6c95aa1
kelvinobuvie <kelvinobuvie@gmail.com>
2025-09-20 22:12:20 +0100

    again

--- task_4.sql
+++ task_4.sql
@@ -1,2 +1,7 @@
 -- task_4.sql
-SHOW CREATE TABLE Books;
+USE alx_book_store;
+
+SELECT COLUMN_NAME, COLUMN_TYPE
+FROM INFORMATION_SCHEMA.COLUMNS
+WHERE TABLE_SCHEMA = 'alx_book_store'
+  AND TABLE_NAME = 'Books';
